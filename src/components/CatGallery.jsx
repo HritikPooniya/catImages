579f05c518770fc9ef744658e888a4f48a60ca87
Hritik Pooniya <pooniyarpsingh@gmail.com>
2025-03-21 15:48:27 +0530

    Add files via upload

--- src/components/CatGallery.jsx
+++ src/components/CatGallery.jsx
@@ -13,7 +13,11 @@ const CatGallery = () => {
     redirect: 'follow'
   };
 
+  const [cats, setCats] = useState([]);
+  const [loading, setLoading] = useState(false);  
+
   const fetchCatData = async () => {
+    setLoading(true);  
     try {
       const requests = Array.from({ length: 6 }, () => 
         fetch("https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1", requestOptions)
@@ -21,21 +25,22 @@ const CatGallery = () => {
       );
 
       const results = await Promise.all(requests);
-      const newCats = results.flat(); // Flatten the array if needed
-      setCats(prevCats => [...prevCats, ...newCats]); // Append new images
+      const newCats = results.flat();
+      setCats(prevCats => [...prevCats, ...newCats]);
     } catch (error) {
       console.log('Error fetching cat data:', error);
+    } finally {
+      setLoading(false); 
     }
   };
-
-  const [cats, setCats] = useState([]);
 
   useEffect(() => {
     fetchCatData();
   }, []);
 
   return (
-    <div>
+    <div className="text-center">
+        
 
 <LoadMoreButton onClick={fetchCatData} />
       <div className="grid grid-cols-3 gap-4">
@@ -43,6 +48,8 @@ const CatGallery = () => {
           <img key={cat.id} src={cat.url} alt="Cat" className="rounded-lg" width={300} />
         ))}
       </div>
+
+      {loading && <h2 className="text-lg font-semibold text-center">Loading...</h2>}
       <LoadMoreButton onClick={fetchCatData} />
     </div>
   );
